5db913731ccbb48da4baa5889bd2efcc1776df62
Madalyn Querubin <iwa.maddy@gmail.com>
2024-09-01 00:31:27 -0600

    clean up on line breaks

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,4 +1,4 @@
 print("Welcome to BandStorm, the Band name generator that will help you brainstorm a name for your band!")
-city = input("What city did you grow up in? ")
-pet = input("What is the name of your favorite pet? ")
-print(city + " " + pet)
+city = input("What city did you grow up in?\n ")
+pet = input("What is the name of your favorite pet?\n ")
+print("Your band name could be... " + city + " " + pet + "!")
