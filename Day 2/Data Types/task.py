dd3c8c0dab4a85d139cca14c1e15b8879a4773ac
Madalyn Querubin <iwa.maddy@gmail.com>
2024-09-01 00:57:50 -0600

    day2_prt1_data_types_notes

--- Day 2/Data Types/task.py
+++ Day 2/Data Types/task.py
@@ -1,1 +1,18 @@
-len("Hello")
+#subscripting, telling the computer which character to print. we always start at 0, printing [0] will print "H"
+print("Hello"[0])
+print("Hello"[4])
+# can also use [-1] or negative numbers, to count backwards. if you put [-1] it will print the last character of the string
+
+#Integer = whole numbers
+# use " " for strings, it will print what you type, for whole numbers to act like numbers, no need for " "
+print(123 + 123)
+
+#Large Integers, can use underscores instead of commas -- 1,000 --- 1_000, computer will ignore
+print(1_000_000)
+
+#float = floating point number
+print(3.14159)
+
+#boolean, True/False, always capitalized
+print(True)
+print(False)
