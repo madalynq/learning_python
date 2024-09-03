49060fbfc2a07e64526778e806ecc8629650aff6
Madalyn Querubin <iwa.maddy@gmail.com>
2024-09-02 23:22:35 -0600

    day2_prt3_mathematical_operations

--- Day 2/Mathematical Operations/task.py
+++ Day 2/Mathematical Operations/task.py
@@ -1,1 +1,19 @@
 print("My age: " + str(12))
+print(123 + 456)
+print(7 - 3)
+print(3 * 2)
+print(6 / 3)      #python naturally prints division into floats
+print(6 // 3)     #this overwrites it, will come out in int, but will take off all decimals (not as accurate)
+print(2 ** 2)     #exponents
+
+# PEMDAS
+# paretheses
+# exponents
+# multiplication or division (whichever happens first when reading th equation from left to right)
+# addition or subtraction    (same rule as above)
+
+print(3 * 3 + 3 / 3 - 3)
+
+# change line of code to make it output 3 instead of 7
+
+print(3 * (3 +3) / 3 - 3 )
