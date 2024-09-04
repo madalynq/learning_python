3b29efd800d0478d294db3ddabc7a636c6907eaa
Madalyn Querubin <iwa.maddy@gmail.com>
2024-09-04 11:04:53 -0600

    day3_prt1_if_else

--- Day 3/If Else/task.py
+++ Day 3/If Else/task.py
@@ -1,2 +1,12 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+
+if height >=120:
+    print("Enjoy your ride!")
+
+else:
+    print("Sorry, you must be at least 120cm to ride")
+
+#  >=, <=  ... greater/ lesser than OR equal to
+#   ==     ... equal to
+#   !=     ... not equal to
