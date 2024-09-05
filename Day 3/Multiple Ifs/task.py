7b975f97adece2b4d752575dd448db80a1ca662a
Madalyn Querubin <iwa.maddy@gmail.com>
2024-09-05 15:19:13 -0600

    day3_prt4_multiple_ifs

--- Day 3/Multiple Ifs/task.py
+++ Day 3/Multiple Ifs/task.py
@@ -1,14 +1,22 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+bill = 0
 
 if height >= 120:
     print("You can ride the rollercoaster")
     age = int(input("What is your age? "))
     if age <= 12:
-        print("Please pay $5.")
+        bill = 5
+        print("Child tickets are $5.")
     elif age <= 18:
-        print("Please pay $7.")
+        bill = 7
+        print("Youth tickets are $7.")
     else:
-        print("Please pay $12.")
+        bill = 12
+        print("Adult tickets are $12.")
+    wants_photo = input("Do you want a photo? y for yes and n for no. ")
+    if wants_photo == "y":
+        bill += 3                                                            ####this quicker code to add to value of variable
+    print(f"Your total today will be ${bill}")
 else:
     print("Sorry you have to grow taller before you can ride.")
