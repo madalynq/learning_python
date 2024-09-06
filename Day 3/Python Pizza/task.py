437adacccf33860bb84d1b7d2efd8401a908e4d2
Madalyn Querubin <iwa.maddy@gmail.com>
2024-09-05 23:07:58 -0600

    day3_prt5.1_python_pizza

--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -1,5 +1,19 @@
 print("Welcome to Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M or L: ")
-pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
-extra_cheese = input("Do you want extra cheese? Y or N: ")
+bill = 0
+if size == "S":
+    bill = 15
+    pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+    if pepperoni == "Y":
+        bill += 2
+    extra_cheese = input("Do you want extra cheese? Y or N: ")
+    if extra_cheese == "Y":
+        bill += 1
+elif size == "M":
+    bill = 20
+
+else:
+    bill = 25
+
+
 
