ada03e272bb0e1d3850682f9f74d1fcb21ec4989
Madalyn Querubin <iwa.maddy@gmail.com>
2024-09-08 23:52:45 -0600

    day4_prt3_bank_roulette

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -1,1 +1,13 @@
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+import random
+randomizer = random.randint(1,5)
+if randomizer == 1:
+    print(friends[0])
+elif randomizer == 2:
+    print(friends[1])
+elif randomizer == 3:
+    print(friends[2])
+elif randomizer == 4:
+    print(friends[3])
+else:
+    print(friends[4])
