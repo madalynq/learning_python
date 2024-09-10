793ef861fab9020d3b31e805ea2303c46af1b3a8
Madalyn Querubin <iwa.maddy@gmail.com>
2024-09-09 23:46:15 -0600

    day4_prt4_notes_finding_items_in_nested_lists

--- Day 4/IndexError/task.py
+++ Day 4/IndexError/task.py
@@ -18,3 +18,15 @@ fruits_and_vegetables = [fruits, vegetables]
 print(fruits_and_vegetables)
 # should look like: [['cherry', 'strawberry', 'apple', 'orange', 'mango'], ['carrot', 'broccoli', 'cabbage', 'peppers']]
 ##double brackets to show the list includes more than one list
+
+print(fruits_and_vegetables[1])
+## will print the second list, or:
+# ['carrot', 'broccoli', 'cabbage', 'peppers']
+###BECAUSE the first list would be list 0, not 1
+
+print(fruits_and_vegetables[1][1])
+# "broccoli"
+## this prints the 1st item in the 1st list, not item 0 in list 0
+
+print(fruits_and_vegetables[0][0])
+# this would print "cherry"
