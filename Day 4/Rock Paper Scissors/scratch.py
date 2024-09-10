0d190e51e81082457c258c88e0403f28d4a4a51c
Madalyn Querubin <iwa.maddy@gmail.com>
2024-09-10 00:40:39 -0600

    day4_prt5_rock_paper_scissors_unfinished

---
+++ Day 4/Rock Paper Scissors/scratch.py
@@ -0,0 +1,21 @@
+if user_choice == 0:
+    if comp_choice == 0:
+        print("Draw")
+    elif comp_choice == 1:
+        print("You Lose")
+    else:
+        print("You Win")
+elif user_choice == 1:
+    if comp_choice == 0:
+        print("You Win")
+    elif comp_choice == 1:
+        print("Draw")
+    else:
+        print("You Lose")
+elif user_choice == 2:
+    if comp_choice == 0:
+        print("You Lose")
+    elif comp_choice == 1:
+        print("You Win")
+    else:
+        print("Draw")
--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -24,3 +24,64 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
+user_choice = (input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors.\n"))
+if user_choice == 0:
+    print(rock)
+elif user_choice == 1:
+    print(paper)
+else:
+    print(scissors)
+
+
+import random
+comp_choice = random.randint(0,2)
+if comp_choice == 0:
+    print(f"Computer chose:\n {rock}")
+elif comp_choice == 1:
+    print(f"Computer chose:\n {paper}")
+else:
+    print(f"Computer chose:\n {scissors}")
+
+
+if user_choice == 0 and comp_choice == 0:
+    print("Draw")
+elif user_choice == 0 and comp_choice == 1:
+    print("You Lose")
+elif user_choice == 0 and comp_choice == 2:
+    print("You Win")
+elif user_choice == 1 and comp_choice == 0:
+    print("You Win")
+elif user_choice == 1 and comp_choice == 1:
+    print("Draw")
+elif user_choice == 1 and comp_choice == 2:
+    print("You Lose")
+elif user_choice == 2 and comp_choice == 0:
+    print("You Lose")
+elif user_choice == 2 and comp_choice == 1:
+    print("You Win")
+elif user_choice == 2 and comp_choice == 2:
+    print("Draw")
+
+
+if user_choice == 0:
+    if comp_choice == 0:
+        print("Draw")
+    elif comp_choice == 1:
+        print("You Lose")
+    else:
+        print("You Win")
+elif user_choice == 1:
+    if comp_choice == 0:
+        print("You Win")
+    elif comp_choice == 1:
+        print("Draw")
+    else:
+        print("You Lose")
+elif user_choice == 2:
+    if comp_choice == 0:
+        print("You Lose")
+    elif comp_choice == 1:
+        print("You Win")
+    else:
+        print("Draw")
