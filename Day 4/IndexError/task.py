08a7fb83d3fba6ac3f63538e267e01b71e5f0aae
Madalyn Querubin <iwa.maddy@gmail.com>
2024-09-09 23:33:15 -0600

    day4_prt4_indexerror_nested_lists

--- Day 4/IndexError/task.py
+++ Day 4/IndexError/task.py
@@ -1,3 +1,5 @@
+
+
 states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland",
                      "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island",
                      "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois",
@@ -5,5 +7,14 @@ states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Conne
                      "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado",
                      "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma",
                      "New Mexico", "Arizona", "Alaska", "Hawaii"]
+#if you try to get an item that is not in the range of a list, you will get an IndexError
+#print(states_of_america[50]) ###"list index is out of range" because there are 50 states but lists start with 0, not 1
 
-print(states_of_america)
+#Nested lists
+
+fruits = ["cherry", "strawberry", "apple", "orange", "mango"]
+vegetables = ["carrot", "broccoli", "cabbage", "peppers"]
+fruits_and_vegetables = [fruits, vegetables]
+print(fruits_and_vegetables)
+# should look like: [['cherry', 'strawberry', 'apple', 'orange', 'mango'], ['carrot', 'broccoli', 'cabbage', 'peppers']]
+##double brackets to show the list includes more than one list
