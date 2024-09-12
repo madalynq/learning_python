277d52071ce2b629eae78c55b2134c9a7791e76e
Madalyn Querubin <iwa.maddy@gmail.com>
2024-09-11 23:00:16 -0600

    day5_prt1_for_loops

--- Day 5/For Loops/task.py
+++ Day 5/For Loops/task.py
@@ -1,1 +1,11 @@
 fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit) ##will print each variable individually
+    print(fruit + " pie") ## prints for each variable, but each step of code under an if must be carried out in
+                          ## order. the previous step called for each variable to be printed individually, and code will
+                          ## take care of everything needed (code) for that specific variable before moving onto the
+                          ## next variable
+    print(fruits)         ######indentation matters. if this is under an if, it will be printed for every variable
+    print("every variable")
+print("BACK TO NORMAL")   ### when you're no longer in the indentation under the if, it goes back no normal code
+                          ## loop is ended and its back to regular code
