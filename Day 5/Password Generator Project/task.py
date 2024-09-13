d61b0bd2b6bb76388f4cd595b14af1b4708e4aab
Madalyn Querubin <iwa.maddy@gmail.com>
2024-09-12 23:52:12 -0600

    day5_part4_password_generator_attmpt1

--- Day 5/Password Generator Project/task.py
+++ Day 5/Password Generator Project/task.py
@@ -1,3 +1,5 @@
+import random
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -6,5 +8,29 @@ print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password = []
+
+for letter in range(0, nr_letters):
+    qwe = random.choice(letters)
+    password.extend(qwe)
+
+
+for number in range(0,nr_numbers):
+    rty = random.choice(numbers)
+    password.extend(rty)
+
+print(password)
+
+
+
+
+
+
+
+
+
+# print(random.choice(letters) + random.choice(numbers) + random.choice(symbols))
+
 
 
