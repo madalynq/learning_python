964a677b0e937f32f3cfe5c88e8a25affd115be0
Madalyn Querubin <iwa.maddy@gmail.com>
2024-09-14 23:22:59 -0600

    day6_prt1_functions

---
+++ Day 6/Functions/task.py
@@ -0,0 +1,19 @@
+def my_function():
+    print("hello")
+    print("bye")
+
+my_function()      ####"calling" the function
+
+
+### this allows a block of code to all be carried out at once. shorthand, in a way. calling a function can simplify code
+### rather than typing each individual step out
+
+### creating code for Reeborg's world, challenge is to get the robot to turn right when there is no "right turn" in
+### the code, create a function that carries out the process.
+
+
+# def turn_right():
+#   turn_left()
+#   turn_left()
+#   turn_left()
+# turn_right()
