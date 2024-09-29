6ea8878549958fd018c5022f023eba2994b96b28
Madalyn Querubin <iwa.maddy@gmail.com>
2024-09-28 23:41:22 -0600

    day8_prt2_positional_vs_keyword_arguments

--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -1,8 +1,15 @@
-# Functions with input
+# Functions with multiple inputs
 
-def greet_with_name(name):
+def greet_with_name(name, location):
     print(f"Hello {name}")
     print(f"How do you do {name}?")
+    print(f"What is it like in {location}?")
 
 
-greet_with_name("Jack Bauer")
+greet_with_name("Jack Bauer", "New York")
+##positional arguments: the order you put the arguments in matter, the argument is assigned to the variables, or parameters
+## in order of placement in the function
+
+
+##keyword arguments: specify which is which in the function
+greet_with_name(location="New York", name="Jack Bauer")
