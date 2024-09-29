bf6a154bf9748905ab64f7ce1f10aff21d5f0ffc
Madalyn Querubin <iwa.maddy@gmail.com>
2024-09-28 23:28:11 -0600

    day8_prt1_challenge_life_in_weeks

--- Day 8/Functions with Inputs/task.py
+++ Day 8/Functions with Inputs/task.py
@@ -15,3 +15,16 @@ greet_with_name("Madalyn")
 ##is the parameter) NAME OF DATA
 ##what the variable puts out is the argument (ex//placing "madalyn" in the function when calling it is the argument)
 ##ACTUAL VALUE OF DATA
+
+
+#challenge: create a function that calculates how many weeks you have left to live if we live until 90. have the end
+#result print "You have x weeks left."
+# example input: 56
+# example output: You have 1768 weeks left.
+
+def life_in_weeks(age):
+    years_left = 90 - age
+    weeks_left = years_left * 52
+    print(f"You have {weeks_left} weeks left.")
+
+life_in_weeks(56)
