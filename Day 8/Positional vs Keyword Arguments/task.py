ae0124c93fe2b4fcaf6f85b606bdd3a4c3acf524
Madalyn Querubin <iwa.maddy@gmail.com>
2024-11-11 22:00:03 -0700

    day8_calculate_love_score_challenge

--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -17,3 +17,24 @@ greet_with_name(location="New York", name="Jack Bauer")
 ##love calculator exercise:
 #enter two people's names, take the number of times the letters in TRUE appear in both names, and LOVE.
 #concatonate numbers to get love score
+
+def calculate_love_score(name1, name2):
+    combined_names = name1 + name2
+    lower_names = combined_names.lower()
+
+    t = lower_names.count("t")
+    r = lower_names.count("r")
+    u = lower_names.count("u")
+    e = lower_names.count("e")
+    first_digit = t + r + u + e
+
+    l = lower_names.count("l")
+    o = lower_names.count("o")
+    v = lower_names.count("v")
+    e = lower_names.count("e")
+    second_digit = l + o + v + e
+
+    score = int(str(first_digit) + str(second_digit))
+    print(score)
+
+calculate_love_score("kanye west","kim kardashian")
