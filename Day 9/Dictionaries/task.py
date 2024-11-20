e2cff2bf6a8d8e52c43021a3e0d9d5789f5cfe44
Madalyn Querubin <iwa.maddy@gmail.com>
2024-11-20 00:09:41 -0700

    day9_prt1

--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,1 +1,26 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again."
+}
+
+#print(programming_dictionary)
+
+#adding to the dictionary
+programming_dictionary["Loop"] = "The action of doing something over and over again"
+
+empty_dictionary = {}
+
+
+#wipe an existing dictionary
+#programming_dictionary = {}
+
+#edit existing value
+#if there is no key that matches, it will add a new one
+programming_dictionary["Bug"] = "A moth in your computer"
+
+#loop through dictionary
+for thing in programming_dictionary:
+    print(thing)
+    #only prints keys, should put 'for key in dictionary'
+    print(programming_dictionary[thing])
+    #will include values
