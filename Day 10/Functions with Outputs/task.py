6a833bd25874726b8a3ce188adb44a217697e218
Madalyn Querubin <iwa.maddy@gmail.com>
2024-12-02 00:02:16 -0700

    notes_day10_prt1

--- Day 10/Functions with Outputs/task.py
+++ Day 10/Functions with Outputs/task.py
@@ -8,3 +8,14 @@ formatted_string = format_name("anGEla", "YU")
 
 
 
+def function_1(text):
+    return text + text
+# if we placed "hello" into this function and then printed the return, we would get "hellohello"
+def function_2(text):
+    return text.title()
+
+output = function_2(function_1("hello"))
+# you can nest functions using returns because it stores/ keeps the output in the return.
+# saving to a new variable and printing:
+print(output)
+# should return "Hellohello" because the actions of both functions were carried out
